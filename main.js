2f94bc0b31f1b0a62366eb25ad7029c970a29716
Mari <marianeaurora@gmail.com>
2023-01-06 18:44:35 -0300

    Laço com for

--- main.js
+++ main.js
@@ -4,24 +4,16 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0 ; contador  < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = listaDeTeclas[contador].classList[1];
 
-    console.log(instrumento);
-
-    //template string
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function(){
         tocaSom(idAudio)
     }
-
-    contador = contador + 1;
-
-    console.log(contador);
 
 }
