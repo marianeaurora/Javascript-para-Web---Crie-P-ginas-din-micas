93a88894b82afd84265072e1cc991d573aa65ee4
Mari <marianeaurora@gmail.com>
2023-01-06 19:36:52 -0300

    Projeto Finalizado

--- main.js
+++ main.js
@@ -9,11 +9,21 @@ for (let contador = 0 ; contador  < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador]
     const instrumento = listaDeTeclas[contador].classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function(){
         tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        if(evento.code === 'Space' ||evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+        
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
     }
 
 }
